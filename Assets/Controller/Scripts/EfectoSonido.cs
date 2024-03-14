53e885ed6607f29703241899f7ad1837f0d40dab
Ricky Mosquera <58889531+FabianMosquera@users.noreply.github.com>
2024-03-14 13:29:52 -0500

    [Fix] Problems with Audio

---
+++ Assets/Controller/Scripts/EfectoSonido.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EfectoSonido : MonoBehaviour
+{
+    [SerializeField] private AudioClip semilla;
+    [SerializeField] private AudioClip Caida;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player"))
+        {
+            ControladorSonidos.Instance.EjecutarSonido(semilla);
+            Destroy(gameObject);
+        }
+        else
+        {
+            ControladorSonidos.Instance.EjecutarSonido(Caida);
+        }
+    }
+    
+}
