3c02a3cd49082d6c5f53996c713a112074e258bc
Ricky Mosquera <58889531+FabianMosquera@users.noreply.github.com>
2024-04-10 17:53:19 -0500

    [ADD] Killzone

--- Assets/Controller/Scripts/Inventario.cs
+++ Assets/Controller/Scripts/Inventario.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,8 +6,7 @@ public class Inventario : MonoBehaviour
 {
     public List<GameObject> Bag = new List<GameObject>();
     public GameObject inv;
-    public bool Activar_inv;
-    public Text mensajeText; // Referencia al objeto de texto donde se mostrará el mensaje de la semilla
+    public bool activar_inv;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,42 +18,9 @@ public class Inventario : MonoBehaviour
                 {
                     Bag[i].GetComponent<Image>().enabled = true;
                     Bag[i].GetComponent<Image>().sprite = collision.GetComponent<SpriteRenderer>().sprite;
-                    MostrarMensajeSemilla("¡Has recogido una semilla!");
                     break;
                 }
             }
-        }
-    }
-
-    private void MostrarMensajeSemilla(string mensaje)
-    {
-        // Mostrar el mensaje en el objeto de texto
-        mensajeText.text = mensaje;
-        // Después de un tiempo, eliminar el mensaje
-        StartCoroutine(EliminarMensaje());
-    }
-
-    private IEnumerator EliminarMensaje()
-    {
-        // Esperar unos segundos antes de eliminar el mensaje
-        yield return new WaitForSeconds(3);
-        mensajeText.text = ""; // Limpiar el texto después de unos segundos
-    }
-
-    // Método llamado cuando se presiona el botón de activar/desactivar inventario
-    private void Update()
-    {
-        if (Activar_inv)
-        {
-            inv.SetActive(true);
-        }
-        else
-        {
-            inv.SetActive(false);
-        }
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            Activar_inv = !Activar_inv;
         }
     }
 }
--- Assets/Controller/Scripts/ScriptPause.cs
+++ Assets/Controller/Scripts/ScriptPause.cs
@@ -4,7 +4,28 @@ using UnityEngine;
 
 public class ScriptPause : MonoBehaviour
 {
-    public void Pausa(){
-        Time.timeScale = 0f;
+    private bool juegoPausado = false; // Variable para rastrear si el juego está pausado o no
+    [SerializeField] private GameObject menuinv;
+    [SerializeField] private GameObject botoninv;
+
+    public void TogglePausa()
+    {
+        // Alternar entre pausar y reanudar el juego
+        juegoPausado = !juegoPausado;
+        if (juegoPausado)
+        {
+            Time.timeScale = 0f; // Pausar el juego
+        }
+        else
+        {
+            Time.timeScale = 1f; // Reanudar el juego
+        }
+    }
+    public void ToggleMochila()
+    {
+        bool estadoActual = menuinv.activeSelf;
+        menuinv.SetActive(!estadoActual);
+
+        botoninv.SetActive(true);
     }
 }
