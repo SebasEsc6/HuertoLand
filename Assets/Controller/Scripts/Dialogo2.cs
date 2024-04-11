589935a1281e6b4cfb901685ce600bd3bec86ad9
unknown <mauricio.escobar@uao.edu.co>
2024-04-10 23:17:03 -0500

    Mensajes conversacion implementados y ajustes boton pausa

---
+++ Assets/Controller/Scripts/Dialogo2.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Dialogo2 : MonoBehaviour
+{
+
+    public TextMeshProUGUI dialogueText;
+    public string[] lines;
+    public float textSpeed = 0.1f;
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueText.text = string.Empty;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dialogueText.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = lines[index];
+            }
+        }
+    }
+
+
+    public void StartDialogue()
+    {
+        index = 0;
+
+        StartCoroutine(WriteLine());
+    }
+
+    IEnumerator WriteLine()
+    {
+        foreach (char letter in lines[index].ToCharArray()) 
+        {
+            dialogueText.text += letter;
+
+            yield return new WaitForSeconds(textSpeed); 
+        }
+    }
+
+    public void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            dialogueText.text = string.Empty;
+            StartCoroutine(WriteLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+}
+
