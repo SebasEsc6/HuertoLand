fddeda489f19ba0e86f297c88e41ecd88bb43567
Ricky Mosquera <58889531+FabianMosquera@users.noreply.github.com>
2024-05-26 16:42:50 -0500

    [ADD] Carteles y acomodando la fruta en el nivel de la montaña

--- Assets/Model/Inventario.cs
+++ Assets/Model/Inventario.cs
@@ -7,6 +7,8 @@ public class Inventario : MonoBehaviour
 {
     public GameObject inv;
     public bool activar_inv;
+    public GameObject panel;
+    public bool activar_panel;
 
     public void Visible()
     {
@@ -14,11 +16,15 @@ public class Inventario : MonoBehaviour
         {
             inv.SetActive(false);
             activar_inv = false;
+            panel.SetActive(false);
+            activar_panel = false;
         }
         else 
         {
             inv.SetActive(true);
             activar_inv = true;
+            panel.SetActive(true);
+            activar_panel = true;
         }
     }
 }
